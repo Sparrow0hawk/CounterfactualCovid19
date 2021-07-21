b512104b5a753523eaf23f905e5250f6b41dd8b2
Alex Coleman <alexjcoleman@gmail.com>
2021-07-21 14:23:55 +0100

    added python decouple and implemented it in settings

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "3cgwsqthjn#8na&6!=8i*=&ko5x4ti_=fg4%n+rxtq2+ha8a29"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,9 +86,9 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "counterfactualcovid",
-        "USER": "django",
-        "PASSWORD": "django",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWD"),
         "HOST": "localhost",
     }
 }
